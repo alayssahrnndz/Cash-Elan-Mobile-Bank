bf852f20d4265a533ed5ec557de4e6d0fab03472
Alayssa Hernandez <alyhrnndz5@gmail.com>
2025-05-29 22:55:03 +0800

    Update FirebaseConfig.ts

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -6,13 +6,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB7NyfdQKU6Q28Ut7H9wFeHYqELGwIChTY",
-  authDomain: "mbank-32d6d.firebaseapp.com",
-  projectId: "mbank-32d6d",
-  storageBucket: "mbank-32d6d.firebasestorage.app",
-  messagingSenderId: "941323560039",
-  appId: "1:941323560039:web:16084fe9d0b3b186d2e0c2",
-  measurementId: "G-ZCJBDGT2EV"
+  apiKey: "YOUR API KEY",
+  authDomain: "YOUR AUTH DOMAIN",
+  projectId: "YOUR PROJECT ID",
+  storageBucket: "INPUT HERE",
+  messagingSenderId: "INPUT HERE",
+  appId: "INPUT HERE",
+  measurementId: "INPUT HERE"
 };
 
 // Initialize Firebase
